7f95f1f8fa5be1c75521e40c5e8e9b26d9a716db
Pedro Roberto <pedrofuruguem@hotmail.com>
2022-10-26 18:42:05 -0300

    Solicitação 2 - Quest finalizada

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -7,54 +7,47 @@ const screen = {
                                         <div class="data"> 
                                             <h1>${user.name ?? 'não possui nome cadastrado 😢'}</h1>
                                             <p>${user.bio ?? 'não possui bio cadastrada 😢'}</p>
-                                            <p>seguidores: ${user.followers}</p>
+                                            <p>seguidores: ${user.followers}</p> 
                                             <p>seguindo: ${user.following}</p>
                                         </div>
                                     </div>`
-            //document.querySelector('.profile-data').innerHTML = userInfo
+           
         let repositoriesItens = ''
         user.repositories.forEach(repo => repositoriesItens += `<li>
                                                                 <a href="${repo.html_url}" target="_blank">${repo.name}<br>
-                                                                forks: ${repo.forks_count}<br>
-                                                                estrelas: ${repo.stargazers_count}<br> 
-                                                                watchers: ${repo.watchers_count}<br>
+                                                                forks: ${repo.forks}<br>
+                                                                estrelas: ${repo.stargazers_count}<br>
+                                                                watchers: ${repo.watchers}<br>
                                                                 linguagem: ${repo.language}
                                                                 </a>
                                                                 </li>`)
         
         //console.log(repositoriesItens)
-
         if(user.repositories.length > 0){
             this.userProfile.innerHTML += `<div class="repositories section"> 
                                               <h2>Repositórios</h2>
                                               <ul>${repositoriesItens}</ul>
                                            </div>`
         }
-
-        
+        //SOLICITAÇÃO 2
+        const filtered = user.events.filter(f => f.type === 'PushEvent' ||  f.type === 'CreateEvent' && f.payload.commits === 'message')
+        console.log(filtered)
+    
         let eventsItens = ''
-
-        user.events.forEach(e => eventsItens += `<li><a>${e.repo.name}/${e.payload.commits}</a></li>`)
-
-        if(user.events.length > 0 ){
-            this.userProfile.innerHTML +=  `<div class="events section">
-                                             <h2>Eventos</h2>
-                                             <ul>${eventsItens}</ul>
+        filtered.forEach(e => eventsItens += `<li><a>${e.repo.name} - ${e.payload.commits[0].message}</a></li>`)
+           
+        if(user.events.length > 0){   
+            this.userProfile.innerHTML +=   `<div class="events section">
+                                                <h2>Eventos</h2>
+                                                <ul>${eventsItens}</ul>
                                             </div>`
         }
-        console.log(eventsItens)
-
-    },
-    renderNotFound(){
-        this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
-    }
+        
+        },
+        renderNotFound(){
+            this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
+        }
 }
 
 export { screen }
 
-/*
-se tu observar esse array que retorna, nem todos payloads terão commits, nisso tu deve realizar um filtro
-[Imagem]
-a partir disso, poderá percorrer os commits e mostrar a mensagem de cada um
-se atenta que ele é um array também
-*/
